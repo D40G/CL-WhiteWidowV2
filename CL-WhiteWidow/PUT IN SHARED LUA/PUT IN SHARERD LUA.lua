d2c510fd44582b89ef35905d39c37fbbd5ca774e
D40G <toofy0@yahoo.com>
2022-03-10 16:36:01 -0500

    cleanup+ added isboss in the boss role

--- CL-WhiteWidow/PUT IN SHARED LUA/PUT IN SHARERD LUA.lua
+++ CL-WhiteWidow/PUT IN SHARED LUA/PUT IN SHARERD LUA.lua
@@ -1,27 +1,19 @@
-["cannabis"] 						 = {["name"] = "cannabis", 			 	  	  	["label"] = "Raw Cannabis", 			["weight"] = 450, 		["type"] = "item", 		["image"] = "cannabis.png", 				["unique"] = false, 		["useable"] = true, 	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "Raw Cannabis"},
-["skunk"] 						 = {["name"] = "skunk", 			 	  	  	["label"] = "Raw Skunk", 			["weight"] = 450, 		["type"] = "item", 		["image"] = "weedskunk.png", 				["unique"] = false, 		["useable"] = true, 	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "Raw Skunk"},
-["ak-47"] 						 = {["name"] = "ak-47", 			 	  	  	["label"] = "Raw AK-47", 			["weight"] = 450, 		["type"] = "item", 		["image"] = "weedak47.png", 				["unique"] = false, 		["useable"] = true, 	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "Raw AK-47"},
-["og-kush"] 						 = {["name"] = "og-kush", 			 	  	  	["label"] = "Raw Og-Kush", 			["weight"] = 450, 		["type"] = "item", 		["image"] = "weedogkush.png", 				["unique"] = false, 		["useable"] = true, 	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "Raw Og-Kush"},
-["rollingpaper"] 						 = {["name"] = "rollingpaper", 			 	  	  	["label"] = "Rolling Paper For Joints", 			["weight"] = 450, 		["type"] = "item", 		["image"] = "rollingpaper.png", 				["unique"] = false, 		["useable"] = true, 	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "Rolling Paper For Joints"},
-["bong"] 			         = {["name"] = "bong", 			      	["label"] = "Bong", 			["weight"] = 1000, 		["type"] = "item", 		["image"] = "bong.png", 	["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,	   ["combinable"] = nil,   ["description"] = "Bong"},
-["weedscissors"] 			         = {["name"] = "weedscissors", 			      	["label"] = "Weed Scissors", 			["weight"] = 1000, 		["type"] = "item", 		["image"] = "weedscissors.png", 	["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,	   ["combinable"] = nil,   ["description"] = "Weed Scissors"},
+--items.lua
+["cannabis"] 					= {["name"] = "cannabis", 			["label"] = "Raw Cannabis", 			["weight"] = 450, 		["type"] = "item", 		["image"] = "cannabis.png", 	["unique"] = false, 	["useable"] = true, 	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "Raw Cannabis"},
+["skunk"] 						= {["name"] = "skunk", 			 	["label"] = "Raw Skunk", 			    ["weight"] = 450, 		["type"] = "item", 		["image"] = "weedskunk.png", 	["unique"] = false, 	["useable"] = true, 	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "Raw Skunk"},
+["ak-47"] 						= {["name"] = "ak-47", 			 	["label"] = "Raw AK-47", 			    ["weight"] = 450, 		["type"] = "item", 		["image"] = "weedak47.png", 	["unique"] = false, 	["useable"] = true, 	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "Raw AK-47"},
+["og-kush"] 					= {["name"] = "og-kush", 			["label"] = "Raw Og-Kush", 			    ["weight"] = 450, 		["type"] = "item", 		["image"] = "weedogkush.png", 	["unique"] = false, 	["useable"] = true, 	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "Raw Og-Kush"},
+["rollingpaper"] 				= {["name"] = "rollingpaper", 		["label"] = "Rolling Paper For Joints", ["weight"] = 450, 		["type"] = "item", 		["image"] = "rollingpaper.png", ["unique"] = false, 	["useable"] = true, 	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "Rolling Paper For Joints"},
+["bong"] 			            = {["name"] = "bong", 			    ["label"] = "Bong", 			        ["weight"] = 1000, 		["type"] = "item", 		["image"] = "bong.png", 	    ["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,	   ["combinable"] = nil,   ["description"] = "Bong"},
+["weedscissors"] 			    = {["name"] = "weedscissors", 		["label"] = "Weed Scissors", 			["weight"] = 1000, 		["type"] = "item", 		["image"] = "weedscissors.png", ["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,	   ["combinable"] = nil,   ["description"] = "Weed Scissors"},
 
-
+--jobs.lua
 ["whitewidow"] = {
-    label = "White Widow",
-    defaultDuty = true,
-    grades = {
-        ['0'] = {
-            label = 'Worker',
-            payment = 30,
-        },
-        ['1'] = {
-            label = 'Vice Boss',
-            payment = 70,
-        },
-        ['2'] = {
-            label = 'Boss',
-            payment = 130,
-        },
-    }
-},
+		label = "White Widow",
+		defaultDuty = false,
+		grades = {
+			['0'] = { name = 'Worker',	payment = 30, },
+			['1'] = { name = 'Vice Boss', payment = 70, },
+			['2'] = { name = 'Boss', isboss = true, payment = 150 },
+		}
+	},
